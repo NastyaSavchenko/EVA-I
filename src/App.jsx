450b781d5aa68fe1675d4181c531774ba055eab6
Анастасія Савченко <savyle4ka13@gmail.com>
2023-07-19 12:24:39 +0100

    add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
 import { Home } from "./pages/Home/Home";
 import { Examples } from "./pages/Examples/Examples";
 import { Pricing } from "./pages/Pricing/Pricing";
+import { Layout } from "./components/Layout/Layout";
 
-import { Navigate, Route, Router } from "react-router-dom";
+
 
 function App() {
-  return <>
-App
-  
-  </>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="examples" element={<Examples />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<Navigate to={"/"} />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
