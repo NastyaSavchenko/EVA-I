f3c2414e4146a25075c829ab6b6b0f4f67d2ec02
Анастасія Савченко <savyle4ka13@gmail.com>
2023-07-20 13:46:53 +0100

    add MainNav Component

--- src/components/MainNav/MainNav.styled.jsx
+++ src/components/MainNav/MainNav.styled.jsx
@@ -1,1 +1,23 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const NavLinkStyles = styled(NavLink)`
+  padding: 4px 8px;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 1.16;
+  color: var(--text-grey);
+
+  &:not(:last-child) {
+    margin-right: 32px;
+  }
+
+  &:hover,
+  &:focus {
+    color: var(--main-black);
+  }
+
+  &.active {
+    color: var(--main-black);
+  }
+`;
