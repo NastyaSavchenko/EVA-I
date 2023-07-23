0a3d07cf6a33ef36e87c6dca8c08903096298375
Ir-ra <chapter.ku@ukr.net>
2023-07-23 17:14:40 +0300

    add usage examples + add TryBot comp to Examples page

---
+++ src/components/Usage/Usage.styled.jsx
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+
+export const UsageStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 100px 190px;
+  font-style: normal;
+`
+export const UsageTitle = styled.h1`
+  text-align: center;
+  font-size: 40px;
+  font-weight: 600;
+  line-height: 80px; 
+  margin-bottom: 80px;
+`
+export const UsageCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 34px;
+  row-gap: 64px;
+  justify-items: center;
+`
+export const UsageCard = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 24px;
+`
+export const CardImg = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  /* width: auto; */
+  width: 515px;
+  height: 352px;
+  padding: 10px;
+  border: 1px solid grey;
+`
+export const CardInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`
+export const CardTop = styled.div`
+  display: flex;
+  gap: 24px;
+`
+export const CardIcon = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 80px;
+  height: 80px;
+  padding: 10px;
+
+  gap: 10px;
+  border: 1px solid grey;
+  border-radius: 50%;
+`
+export const CardTitle = styled.p`
+  display: flex;
+  align-items: center;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 40px;
+`
+export const CardText = styled.p`
+  width: 515px;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 20px;
+`
