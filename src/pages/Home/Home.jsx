2986ba2d7be5c48d6c5c02a37ee3ce91bd403888
Ir-ra <chapter.ku@ukr.net>
2023-08-02 18:57:13 +0300

    sign-in page/fixes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,12 @@ import { FAQ } from "../../components/FAQ/FAQ.jsx";
 import { BotBtn } from "../../components/BotBtn/BotBtn.jsx";
 import { Benefits } from "../../components/Benefits/Benefits.jsx";
 import { Hero } from "../../components/Hero/Hero.jsx";
-import { Authentication } from "../Authentication/Authentication.jsx";
 
 
 export const Home = () => {
   return (
     <main>
       <Hero />
-      <Authentication />
       <Advantages />
       <Benefits />
       <Reviews />
