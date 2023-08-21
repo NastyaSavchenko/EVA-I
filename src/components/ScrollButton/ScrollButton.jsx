b044e3a9c679a7dd3a595066ee7c92778ffe7863
Ir-ra <chapter.ku@ukr.net>
2023-08-21 16:17:16 +0300

    change btn position, add styles

--- src/components/ScrollButton/ScrollButton.jsx
+++ src/components/ScrollButton/ScrollButton.jsx
@@ -2,6 +2,7 @@ import { Button } from "./ScrollButton.styled.jsx";
 import { IoIosArrowUp } from "react-icons/io";
 
 export const ScrollButton = () => {
+  let iconStyles = { color: "white", fontSize: "1.5em" };
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -11,7 +12,7 @@ export const ScrollButton = () => {
 
   return (
     <Button type="button" onClick={scrollToTop}>
-      <IoIosArrowUp />
+      <IoIosArrowUp style={iconStyles} />
     </Button>
   );
 };
