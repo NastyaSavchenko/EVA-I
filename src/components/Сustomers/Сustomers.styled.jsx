49d745b53afb6fe8257161f51e5f4a9fb9228511
Анастасія Савченко <savyle4ka13@gmail.com>
2023-08-22 23:12:23 +0100

    add customers component

---
+++ src/components/Сustomers/Сustomers.styled.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const CustomerList = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
+
+export const CustomerItems = styled.li`
+  display: flex;
+  width: 180px;
+  height: 60px;
+  padding: 10px;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 8px;
+  background: #f5f5f5;
+`;
