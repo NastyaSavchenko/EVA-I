6f1ba304240945d1516250029d3d1ff1c31aac5c
Ir-ra <chapter.ku@ukr.net>
2023-08-22 10:30:10 +0300

    botBtn_styles

--- src/components/BotBtn/BotBtn.styled.jsx
+++ src/components/BotBtn/BotBtn.styled.jsx
@@ -5,11 +5,29 @@ export const BotBtnStyles = styled.button`
   bottom: 40px;
   left: calc(100% - 160px);
 
-  width: 80px;
-  height: 80px;
+  display: flex;
+  justify-content: center;
+  width: 64px;
+  height: 64px;
+  gap: 8px;
+
   border-radius: 50%;
   border: none;
   z-index: 1;
 
-  background-color: var(--main-gray);
+  background: var(--light-gradient);
+`;
+
+export const BotContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const BotImg = styled.img`
+  display: flex;
+  align-content: center;
+  width: 52px;
+  height: 52px;
+  padding: 3px 2px;
 `;
