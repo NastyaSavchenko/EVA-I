f11041d7d220951c8e1fff76fa086adb1321a315
Анастасія Савченко <savyle4ka13@gmail.com>
2023-08-22 21:41:41 +0100

    add styles for examples page

--- src/components/Usage/Usage.styled.jsx
+++ src/components/Usage/Usage.styled.jsx
@@ -1,62 +1,79 @@
 import styled from "styled-components";
 
-export const UsageCards = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  column-gap: 34px;
-  row-gap: 64px;
-  justify-items: center;
-  margin-top: 80px;
-`;
-export const UsageCard = styled.div`
-  display: inline-flex;
+export const UsageCards = styled.ul`
+  display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  gap: 24px;
+  align-items: center;
+  margin-top: 80px;
+  margin-bottom: 100px;
+
+  @media (min-width: 1200px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 32px;
+  }
 `;
+
+export const UsageCard = styled.li`
+  max-width: 515px;
+  width: 100%;
+
+  &:not(:last-child) {
+    margin-bottom: 24px;
+  }
+
+  @media (min-width: 1200px) {
+    flex-basis: calc((100% - 2 * 32px) / 2);
+  }
+`;
+
 export const CardImg = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  /* width: auto; */
-  width: 515px;
+  width: 100%;
   height: 352px;
   padding: 10px;
-  border: 1px solid grey;
+  margin-bottom: 24px;
+
+  border-radius: 12px;
+  background-color: var(--light-grey);
 `;
+
 export const CardInfo = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 16px;
 `;
+
 export const CardTop = styled.div`
   display: flex;
   gap: 24px;
+  margin-bottom: 16px;
 `;
-export const CardIcon = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
 
+export const CardIcon = styled.div`
   width: 80px;
   height: 80px;
   padding: 10px;
 
-  gap: 10px;
-  border: 1px solid grey;
+  background-color: var(--light-grey);
+  background-image: url(${(props) => props.bg});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
   border-radius: 50%;
 `;
+
 export const CardTitle = styled.p`
-  display: flex;
-  align-items: center;
   font-size: 24px;
   font-weight: 600;
   line-height: 1.66;
 `;
+
 export const CardText = styled.p`
-  width: 515px;
   font-size: 16px;
   line-height: 1.25;
 `;
